b2fe4c608c7dfaa4a3638ed724f184eb1ae659ff
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-03-08 00:20:28 +0300

    home added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,20 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
+
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState(null);
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData = async()=>{
+    const {data} = await axios(url)
+  }
   return (
     <div>
       
